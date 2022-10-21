3311a3f55ac4b108e3e7f4e3dde96ca54b75f269
DamonAustin <d.a.austin.home@gmail.com>
2022-10-21 00:22:39 -0500

    GameMode Finished - StartGame tests initialized

--- GameLogic/GameLogic.cs
+++ GameLogic/GameLogic.cs
@@ -9,15 +9,30 @@ namespace SOS.GameLogic
     public class Game
     {
         private int boardSize = 3;
+        private bool gameMode = false;
+        private int[,] board = null;
 
-        public bool setBoardSize(int n){
+        public bool setBoardSize(int n) {
             if (n < 3) return false;
             boardSize = n;
             return true;
         }
-        
-        public int getBoardSize(){
+
+        public int getBoardSize() {
             return boardSize;
+        }
+
+        private void createBoard(){
+            board = new int[boardSize, boardSize];
+        }
+
+        public void setGameMode(bool input){
+            gameMode = input;
+        }
+
+        public string getGameMode(){
+            if (!gameMode) return "Simple";
+            return "General";
         }
     }
 }
