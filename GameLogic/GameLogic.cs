966bcee5dacdd5eadf4f115ab11961e66ecf3e91
DamonAustin <d.a.austin.home@gmail.com>
2022-10-20 23:18:55 -0500

    Project Initialized - BoardSize Finished

---
+++ GameLogic/GameLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOS.GameLogic
+{
+    public class Game
+    {
+        private int boardSize = 3;
+
+        public bool setBoardSize(int n){
+            if (n < 3) return false;
+            boardSize = n;
+            return true;
+        }
+        
+        public int getBoardSize(){
+            return boardSize;
+        }
+    }
+}
